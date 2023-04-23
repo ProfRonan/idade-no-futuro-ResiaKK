f82b0e8f469467c6b0204660476cb30ccd06130e
Kelvin Kaiser Sousa Carneiro <kelvin.ksc10@gmail.com>
2023-04-23 10:49:38 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+ano_atual = int(input("Digite o ano atual:  "))
+idade_atual = int(input("Digite sua idade atual:  "))
+outro_ano = int(input("Digite outro ano qualquer:  "))
+nome = input("Digite seu nome:  ")
+idade_futura = outro_ano - ano_atual ++ idade_atual
+
+
+print("{}, no ano de {} você terá {} anos".format(nome, outro_ano, idade_futura))
